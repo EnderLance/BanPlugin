6f3b4d83701e7335f1e16d1a9c75894c933fa63b
EnderLance <blancew@outlook.com>
2013-11-01 14:40:15 -0400

    Fixed messed up indents in logToFile()

--- src/ca/enderlance/banplugin/BanPlugin.java
+++ src/ca/enderlance/banplugin/BanPlugin.java
@@ -25,7 +25,7 @@ public class BanPlugin extends JavaPlugin
 			tplayer.setBanned(false);
 		}
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
 		if(cmd.getName().equalsIgnoreCase("ban"))
@@ -81,39 +81,38 @@ public class BanPlugin extends JavaPlugin
 		}
 		return true;
 	}
-	
-	//Method below from https://forums.bukkit.org/threads/making-a-log-file-for-your-plugins.85430/
-	
-	public void logToFile(String message)
-    {
-        try
-        {
-        	File dataFolder = getDataFolder();
-        	if(!dataFolder.exists())
-            {
-                dataFolder.mkdir();
-            }
-            File saveTo = new File(getDataFolder(), "banLog.txt");
-            if (!saveTo.exists())
-            {
-            	saveTo.createNewFile();
-            }
 
-            FileWriter fw = new FileWriter(saveTo, true);
- 
-            PrintWriter pw = new PrintWriter(fw);
- 
-            pw.println(message);
- 
-            pw.flush();
- 
-            pw.close();
- 
-        } catch (IOException e)
-        {
- 
-        	e.printStackTrace();
- 
-        }
+	//Method below from https://forums.bukkit.org/threads/making-a-log-file-for-your-plugins.85430/
+
+	public void logToFile(String message)
+	{
+        	try
+        	{
+        		File dataFolder = getDataFolder();
+        		if(!dataFolder.exists())
+        		{
+        			dataFolder.mkdir();
+        		}
+        		File saveTo = new File(getDataFolder(), "banLog.txt");
+        		if (!saveTo.exists())
+        		{
+        			saveTo.createNewFile();
+        		}
+
+        		FileWriter fw = new FileWriter(saveTo, true);
+
+		        PrintWriter pw = new PrintWriter(fw);
+
+        		pw.println(message);
+
+			pw.flush();
+			pw.close();
+
+	        } catch (IOException e)
+        	{
+
+	        	e.printStackTrace();
+
+	        }
 	}
 }
